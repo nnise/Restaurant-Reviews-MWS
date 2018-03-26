9db00772d8f902ee1a9c5136c1a9701778b2360e
Dennise Cepeda <28541649+nnise@users.noreply.github.com>
2018-03-26 16:51:34 +0200

    Accesibility
    Image alt attribute

--- js/main.js
+++ js/main.js
@@ -141,6 +141,8 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('aria-label', 'restaurant:' + restaurant.name);
+
   li.append(image);
 
   const name = document.createElement('h1');
