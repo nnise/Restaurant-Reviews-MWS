327aced03afe98f6f6f7e74adcaf7a32e4aa38fa
Dennise Cepeda <28541649+nnise@users.noreply.github.com>
2018-06-28 18:51:26 +0200

    Getting Images
    Fixing photos source

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -155,7 +155,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`img/${restaurant.id}.jpg`);
   }
 
   /**
