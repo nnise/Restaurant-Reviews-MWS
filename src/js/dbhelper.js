100c3c58da3d643af98adb3dc1bef9599c5ca912
Dennise Cepeda <28541649+nnise@users.noreply.github.com>
2018-07-29 16:04:33 +0200

    Review added and showed

--- src/js/dbhelper.js
+++ src/js/dbhelper.js
@@ -14,7 +14,7 @@ const altTags = {
 
 
   /**
-   * Opening & sting up a database using IndexedDB (name, version, updgradeCallback)
+   * Opening & setting up a database using IndexedDB (name, version, updgradeCallback)
    and assign it to a promise
    */
 const dbPromise = idb.open("restaurant-reviews-dtbs", 1 , (upgradeDb) => {
@@ -253,42 +253,44 @@ class DBHelper {
    * Fetch all reviews
    */
 
-  
-  static fetchReviews(callback) {
-      dbPromise
-      .then((db) => {
-        const tx = db.transaction('reviews', 'readwrite');
-        const reviewsStore = tx.objectStore('reviews');
-        return reviewsStore.getAll()
-      })
-      .then((reviews) => {
-        if(reviews.length) {
-          
-          callback(null,reviews) 
-        } else {
-          //fetches all reviews of all restaurants
-          fetch(`${DBHelper.DATABASE_URL}/reviews`)
-      .then((response) => {
-          return response.json();
-          }) 
-      .then((response) => {   
-            const reviews = response;
-            //calls database object
-            dbPromise
-            .then((db) => {
-              const tx = db.transaction('reviews', 'readwrite');
-              const reviewsStore = tx.objectStore('reviews');
-              reviews.forEach(review=>reviewsStore.put(review))
-            }) 
-          callback(null,reviews);
-          })
-        }
-      })
-    }
-     
 
-     static fetchReviewsById(id, callback) {
-      DBHelper.fetchReviews((error, reviews) => {
+   /* Network functions */
+  
+  //getting Server Data
+  static fetchReviews(callback) {
+    dbPromise
+    .then((db) => {
+      const tx = db.transaction('reviews', 'readwrite');
+      const reviewsStore = tx.objectStore('reviews');
+      return reviewsStore.getAll()
+    })
+    .then((reviews) => {
+      if(reviews.length) {
+        callback(null,reviews) 
+      } else {
+          //fetches all reviews of all restaurants
+      fetch(`${DBHelper.DATABASE_URL}/reviews`)
+    .then((response) => {
+          return response.json();
+     }) 
+    .then((response) => {   
+      const reviews = response;
+        //calls database object
+          dbPromise
+          .then((db) => {
+            const tx = db.transaction('reviews', 'readwrite');
+            const reviewsStore = tx.objectStore('reviews');
+            reviews.forEach(review=>reviewsStore.put(review))
+          }) 
+          callback(null,reviews);
+        })
+      }
+    })
+  }
+     
+  //filtering Server Data
+  static fetchReviewsById(id, callback) {
+    DBHelper.fetchReviews((error, reviews) => {
       if (error) {
         callback(error, null);
       } else {
@@ -301,4 +303,90 @@ class DBHelper {
     });
   }
 
- }
+  //adding reviews to Server Data
+  static addAndPostReview(e) {
+  e.preventDefault();
+  const data = {
+    id: Date.now(),
+    restaurant_id: window.location.search.slice(4),
+    name: document.getElementById('name').value,
+    rating: document.getElementById('rating').value,
+    comments: document.getElementById('comments').value
+  };
+  console.log(data);
+  fillReviewsHTML([data]);
+
+  // TODO - save event data locally
+
+  const headers = new Headers({'Content-Type': 'application/json'});
+  const body = JSON.stringify(data);
+  return fetch(`${DBHelper.DATABASE_URL}/reviews`, {
+    method: 'POST',
+    headers: headers,
+    body: body
+  });
+}
+ 
+
+}
+  /**
+   * Adding Reviewsç Offline first, Bacground Sync
+   * based on Build an offline-first, data-driven PWA (codeLab)
+   * https://codelabs.developers.google.com/codelabs/workbox-indexeddb/index.html?index=..%2F..%2Findex#0
+   */
+
+const container = document.getElementById('container');
+const offlineMessage = document.getElementById('offline');
+const noDataMessage = document.getElementById('no-data');
+const dataSavedMessage = document.getElementById('data-saved');
+const saveErrorMessage = document.getElementById('save-error');
+const addReviewButton = document.getElementById('add-review-button');
+
+addReviewButton.addEventListener('click', DBHelper.addAndPostReview);
+
+Notification.requestPermission();
+
+
+  /**
+  /* UI functions
+  */
+
+
+function messageOffline() {
+  // alert user that data may not be current
+  const lastUpdated = getLastUpdated();
+  if (lastUpdated) {
+    offlineMessage.textContent += ' Last fetched server data: ' + lastUpdated;
+  }
+  offlineMessage.style.display = 'block';
+}
+
+function messageNoData() {
+  // alert user that there is no data available
+  noDataMessage.style.display = 'block';
+}
+
+function messageDataSaved() {
+  // alert user that data has been saved for offline
+  const lastUpdated = getLastUpdated();
+  if (lastUpdated) {dataSavedMessage.textContent += ' on ' + lastUpdated;}
+  dataSavedMessage.style.display = 'block';
+}
+
+function messageSaveError() {
+  // alert user that data couldn't be saved offline
+  saveErrorMessage.style.display = 'block';
+}
+
+/* Storage functions */
+
+function getLastUpdated() {
+  return localStorage.getItem('lastUpdated');
+}
+
+function setLastUpdated(date) {
+  localStorage.setItem('lastUpdated', date);
+}
+
+
+
